3a74831efdf78286b109a000bc852bdb64377e48
Jeanne Cazelais <jeannecazelais@icloud.com>
2026-02-18 18:37:47 -0500

    vg1

--- Bloc_A/Bloc_A/VG/VG1.py
+++ Bloc_A/Bloc_A/VG/VG1.py
@@ -14,7 +14,38 @@ def solve(timer: int, recipe: list[str]):
     """
     profit = 0
     ### YOUR CODE GOES HERE ###
+    import math
+    time_for_one_recipe = 1
+    non_prepares = []
+    cut = []
+    boiled = []
+    cooked = []
+    for ingredient in recipe:
+        L = ingredient.split()
+        if len(L) == 1:
+            time_for_one_recipe += 1
+            non_prepares.append(ingredient)
+        else:
+            if L[0] == "boiled":
+                boiled.append(ingredient)
+                time_for_one_recipe += 1
+            elif L[0] == "cooked":
+                cooked.append(ingredient)
+                time_for_one_recipe += 1
+            else:
+                time_for_one_recipe += 5
+                cut.append(ingredient)
 
+    nb_max_recettes = math.floor(timer*2/time_for_one_recipe)
+    nb_max_boiled_unique = math.inf
+    nb_max_cooked_unique = math.inf
 
+    if boiled:
+        nb_max_boiled_unique = ((timer/10)/len(boiled))*4
+    if cooked:
+        nb_max_cooked_unique = ((timer/7)/len(cooked))*2
+
+    nb_recettes = math.floor(min([nb_max_recettes, nb_max_boiled_unique, nb_max_cooked_unique]))
+    profit = round(((len(recipe)+len(non_prepares)+2*len(cut)+4*(len(cooked)+len(boiled)))*(len(recipe)/3))*nb_recettes)
 
     return profit
