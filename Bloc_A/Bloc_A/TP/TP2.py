b1730946d3e94396c84b8deab9a56b542eb75fff
Gravitoriann <pokedivin@gmail.com>
2026-02-18 18:07:05 -0500

    Update TP2.py

--- Bloc_A/Bloc_A/TP/TP2.py
+++ Bloc_A/Bloc_A/TP/TP2.py
@@ -13,6 +13,29 @@ def solve(haiku: str):
     final_haiku = []
     ## YOUR CODE GOES HERE ##
     ## VOTRE CODE VA ICI ##
+    for i in ["'", "-", ".", ",", "!", "?"]:
+        haiku = haiku.replace(i, " ")
+    haiku = haiku.split(" ")
+    line1 = []
+    line2 = []
+    line3 = []
+    for section in haiku:
+        if len("".join(line1)) != 10:
+            if len("".join(line1)) + len(section) <= 10:
+                line1.append(section)
+            else: return "Impossible"
+        elif len("".join(line2)) != 7:
+            if len("".join(line2)) + len(section) <= 7:
+                line2.append(section)
+            else: return "Impossible"
+        elif len("".join(line3)) != 13:
+            if len("".join(line3)) + len(section) <= 13:
+                line3.append(section)
+            else: return "Impossible"
+
+    final_haiku = str(" ".join(line1) + "\n" + " ".join(line2) + "\n" + " ".join(line3))
+
+
 
 
 
