0865770a92d40fa5793604b2b54d381e57652f3f
Gravitoriann <pokedivin@gmail.com>
2026-02-19 12:07:38 -0500

    Update TM1.py

--- Bloc_B/TM/TM1.py
+++ Bloc_B/TM/TM1.py
@@ -11,9 +11,34 @@ def solve(network: list[str], destination: int):
     Returns:
         str: The instructions of the Tramway direction to take
     """
-    instructions = ""
+    instructions = []
+    destination = str(destination)
+    search = network[-1].index(destination)
+    found = (len(network[-1]) / 2)
+    center = (len(network[-1]) / 2)
+    for line in network:
+        for (index,item) in enumerate(line):
+            if item =="/" and line[index+1] == "\\":
+                if (center <= (len(network[-1]) / 2) and index <= (len(network[-1]) / 2)) or (center >= (len(network[-1]) / 2) and index >=(len(network[-1]) / 2)):
+                    if index+1 >= center and search >= center:
+                        found= index+1
+                        if index< search:
+                            instructions.append("R")
+                        else: instructions.append("L")
+                    elif index+1 <= center and search <= center:
+                        found= index+1
+                        if index> search:
+                            instructions.append("L")
+                        else: instructions.append("R")
+        center = found
+
+
+
+
+
 
     
 
-    
-    return instructions
+
+    return "".join(instructions)
+
